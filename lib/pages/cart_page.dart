909d89354b7453ef4e7435e2d9d7cf045b4c1796
PenguinBotOnGithub <penguinbotonline@protonmail.com>
2023-11-27 07:51:03 +0700

    fix: [EAI] Out of range error

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -3,11 +3,14 @@ import 'package:flutter_tugas_pas/controllers/cart_controller.dart';
 import 'package:get/get.dart';
 
 class CartPage extends GetView<CartController> {
+  const CartPage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: ListView.builder(
+            itemCount: controller.products.length,
             shrinkWrap: true,
             itemBuilder: (context, count) => ListTile(
                   leading: Image.network(
