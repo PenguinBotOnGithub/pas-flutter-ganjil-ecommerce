8bc87dde095bec8004155d83c4f3a4cbe48159db
PenguinBotOnGithub <penguinbotonline@protonmail.com>
2023-11-29 14:09:21 +0700

    fix: [EAI] Remove access to notifications page

--- lib/controllers/home_controllers/home_controller.dart
+++ lib/controllers/home_controllers/home_controller.dart
@@ -26,7 +26,7 @@ class HomeController extends GetxController {
   }
 
   void onNotifButtonTap() {
-    Get.toNamed("/notifications");
+    // Get.toNamed("/notifications");
   }
 
   void onSearchTap() {
